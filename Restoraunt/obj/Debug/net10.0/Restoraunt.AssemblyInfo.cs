e42f70f8e8d2f56be50afa61529e134ca500008a
MarchenkoMaximOFKKT <marchenko.maxim@stud.onu.edu.ua>
2026-06-05 19:14:02 +0300

    Додав .gitignore

--- Restoraunt/obj/Debug/net10.0/Restoraunt.AssemblyInfo.cs
+++ Restoraunt/obj/Debug/net10.0/Restoraunt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restoraunt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2473ccf90a6297a4267baad94910cae9faaa627d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restoraunt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restoraunt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
